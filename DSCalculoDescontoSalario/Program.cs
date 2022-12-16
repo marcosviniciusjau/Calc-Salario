4009ff7e63358f760ed2ec75ba14cf4d7fa81755
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-16 19:01:38 -0300

    Primeiro Commit

---
+++ DSCalculoDescontoSalario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DSCalculoDescontoSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+double prestacao, salariobruto;
+Console.WriteLine("Digite seu salário bruto");
+salariobruto = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o valor da prestação");
+prestacao = Convert.ToDouble(Console.ReadLine());
+
+double trinta_porcento_salario = salariobruto * 0.3;
+
+Console.WriteLine("30% de {0} é {1}", salariobruto, prestacao);
+
+if (prestacao > trinta_porcento_salario)
+{
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Empréstimo NEGADO :( ");
+}
+else
+    Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Empréstimo APROVADO :) ");
+            
+        }
